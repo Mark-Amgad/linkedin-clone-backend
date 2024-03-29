e49f43f10322b181d579d6bfeae8967f7fc70cc7
Mark-Amgad <markamgad5@gmail.com>
2024-03-30 00:13:56 +0200

    feat(users): create user dto

--- src/modules/users/dtos/create-user.dto.ts
+++ src/modules/users/dtos/create-user.dto.ts
@@ -1,8 +1,29 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+  Max,
+  Min,
+} from 'class-validator';
 import { IUserOnCreation } from '../interfaces/user.interface';
 
 export class CreateUserDto implements IUserOnCreation {
+  @IsNotEmpty()
+  @IsString()
+  @Min(5)
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStrongPassword()
   password: string;
+
+  @IsString()
+  @IsNotEmpty()
   location: string;
 }
