056ae59289ad6d7d2c8f76c2389496fe7cc3dae8
Mark-Amgad <markamgad5@gmail.com>
2024-03-30 05:29:12 +0200

    service TODOs

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,6 @@
+// TODO: create the service
+// TODO: inject jwtService
+// TODO: inject usersService
+// TODO: install bcrypt
+// TODO: create two main methods (register, signIn)
+// TODO: I might create a function that returns a token and reUse it
