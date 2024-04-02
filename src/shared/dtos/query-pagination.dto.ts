36302d22971aacb79ac9b9f07538cef052d0aa73
Mark-Amgad <markamgad5@gmail.com>
2024-04-02 05:12:01 +0200

    feat(shared): 1. pagination to be required

--- src/shared/dtos/query-pagination.dto.ts
+++ src/shared/dtos/query-pagination.dto.ts
@@ -2,11 +2,11 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNumberString, IsOptional } from 'class-validator';
 
 export class QueryPaginationDto {
-  @ApiProperty({ required: false, default: 1 })
+  @ApiProperty({ required: true, default: 1 })
   @IsNumberString()
   page: number;
 
-  @ApiProperty({ required: false, default: 10 })
+  @ApiProperty({ required: true, default: 10 })
   @IsNumberString()
   limit: number;
 }
