17f0f879599224822b41007cbcee89bd28b56516
Mark-Amgad <markamgad5@gmail.com>
2024-04-02 05:11:46 +0200

    feat(shared): 1. pagination to be required

--- src/shared/dtos/query-pagination.dto.ts
+++ src/shared/dtos/query-pagination.dto.ts
@@ -4,11 +4,9 @@ import { IsNumberString, IsOptional } from 'class-validator';
 export class QueryPaginationDto {
   @ApiProperty({ required: false, default: 1 })
   @IsNumberString()
-  @IsOptional()
-  page?: number;
+  page: number;
 
   @ApiProperty({ required: false, default: 10 })
   @IsNumberString()
-  @IsOptional()
-  limit?: number;
+  limit: number;
 }
