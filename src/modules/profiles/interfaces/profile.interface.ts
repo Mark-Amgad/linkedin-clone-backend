5989734911fb03b5b16704344d785aee44afa73e
Mark-Amgad <markamgad5@gmail.com>
2024-04-12 03:50:43 +0200

    feat(profiles): 1. interfaces 2. models 3. module 4. init files 5. migrations

---
+++ src/modules/profiles/interfaces/profile.interface.ts
@@ -0,0 +1,19 @@
+import { IUser } from 'src/modules/users/interfaces/user.interface';
+import { IBase } from 'src/shared/interfaces/base.interface';
+
+export interface IProfile extends IBase {
+  userId: string;
+  user?: IUser;
+
+  headline?: string;
+  summary?: string;
+  industry?: string;
+  website?: string;
+  avatar?: string;
+  cover?: string;
+
+  numberOfConnections: number;
+}
+
+export interface IProfileOnCreation
+  extends Omit<IProfile, 'id' | 'createdAt' | 'updatedAt'> {}
