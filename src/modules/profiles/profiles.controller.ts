96dfc95f59c324aeae674c53939c76a8e218b2f0
Mark-Amgad <markamgad5@gmail.com>
2024-04-26 03:16:28 +0300

    feat(profiles): 1. get list endpoint

--- src/modules/profiles/profiles.controller.ts
+++ src/modules/profiles/profiles.controller.ts
@@ -1,7 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { PaginationResponseDto } from 'src/shared/dtos/pagination-response.dto';
+import { ProfileDto } from './dtos/base-profile.dto';
+import { FindProfilesListQueryDto } from './dtos/find-profiles-list-query.dto';
+import { ProfilesService } from './profiles.service';
 
 @Controller('profiles')
 @ApiBearerAuth()
 @ApiTags('Profiles')
-export class ProfilesController {}
+export class ProfilesController {
+  constructor(private readonly profilesService: ProfilesService) {}
+  @Get('/list')
+  async getProfilesList(
+    @Query() query: FindProfilesListQueryDto,
+  ): Promise<PaginationResponseDto<ProfileDto[]>> {
+    const { page, limit } = query;
+
+    return await this.profilesService.getProfilesList({ page, limit });
+  }
+}
